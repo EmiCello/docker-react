fdcd6052e2ea2364acfc76e4bab1635609082bd9
Emilia Traczyk <emilia.traczyk@codete.com>
2021-04-16 15:35:36 +0200

    text changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I was deployed!!!
+         Now I am working just perfect!!!
         </p>
         <a
           className="App-link"
